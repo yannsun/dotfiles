093949a8a9d37f1ef3b840792f840e104b62c40b
sunjinliang <sunjinliang@chelun.com>
2017-05-09 13:57:37 +0800

    增加go

--- include/php/install.sh
+++ include/php/install.sh
@@ -6,6 +6,14 @@ sudo apt-get install php7.0-curl;
 sudo apt-get install php7.0-mbstring;
 sudo apt-get install php7.0-xml;
 sudo apt-get install php7.0-zip;
+# 编译安装php
+## 安装zlib和libxml
+sudo apt-get install libxml2*;
+
+wget http://cn.php.net/distributions/php-7.0.18.tar.gz;
+tar -zxvf php-7.0.18.tar.gz;
+cd php-7.0.18/ ;
+./configure;
 
 #安装phpredis扩展
 ##下载和安装phpredis
